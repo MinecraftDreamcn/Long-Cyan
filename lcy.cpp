2f84fa0ed1b0726b6ad1df2b6172e0ca1ee8c9f6
MinecraftDreamCN <158718037+MinecraftDreamcn@users.noreply.github.com>
2024-12-26 13:21:26 +0800

    Update lcy.cpp

--- lcy.cpp
+++ lcy.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main(){
   string s;
   cout<<"LCY Command-tool"<<endl;
-  cout<<"Please update at “https://github.com/MinecraftDreamcn/Long-Cyan” Update"<<endl;
+  cout<<"Please update at https://github.com/MinecraftDreamcn/Long-Cyan"<<endl;
   cout<<"Version 1.0.0 Realese"<<endl;
   cout<<">>>";
-  while(cin>>s){
+  while(getline(cin,s)){
     take(s);
   }
   return 0;
