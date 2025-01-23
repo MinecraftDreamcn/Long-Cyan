b2a4fe7eccec6991aee6c85534df811b496f18e5
MinecraftDreamCN <158718037+MinecraftDreamcn@users.noreply.github.com>
2025-01-23 22:24:12 +0800

    Update lcy.cpp

--- lcy.cpp
+++ lcy.cpp
@@ -1,17 +1,31 @@
 #include<bits/stdc++.h>
+#include<windows.h>
 #include"head/lcystd.h"
 using namespace std;
 int main(){
   string s;
   cout<<"LCY Command-tool"<<endl;
   cout<<"Please update at https://github.com/MinecraftDreamcn/Long-Cyan"<<endl;
-  cout<<"Version 1.0.3 Realese"<<endl;
+  cout<<"Version 1.0.3 Release"<<endl;
   cout<<"Insert 'help' to open help menu.Insert 'exit' to exit program."<<endl;
   cout<<">>>";
   while(getline(cin,s)){
-    if(take(s)==0){
+    if(take(s)==5){
       return 1036;
     }  
   }
-  return 0;
+  cout<<"GOODBYE CTRL+Z USER!"<<endl;
+  Sleep(1000);
+  cout<<"PROGRAM EXIT AFTER 5000MS,PLEASE DON'T WORRY!"<<endl;
+  Sleep(1000);
+  cout<<"PROGRAM BY CONST TEAM(POWERED BY DWS),C++,ÕÔè÷³õ MAKE THIS.THIS IS A EGGSTER!"<<endl;
+  Sleep(1000);
+  cout<<"Sleep(5000);"<<endl;
+  Sleep(1000);
+  cout<<"return 7;"<<endl;
+  Sleep(1000);
+  cout<<"--------------------------------"<<endl;
+	cout<<"Process exited after 8.118 seconds with return value 7"<<endl;
+	system("pause");
+  return 7;
 }
