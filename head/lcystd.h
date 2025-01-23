f7bc7048552c96d0040cdbb64ff31a0ae38380d3
MinecraftDreamCN <158718037+MinecraftDreamcn@users.noreply.github.com>
2025-01-23 20:50:58 +0800

    Update lcystd.h

--- head/lcystd.h
+++ head/lcystd.h
@@ -5,21 +5,22 @@ int take(string s){
 		cout<<">>>";
 	}
 	else if(s=="help"){
-    	cout<<"helplist"<<endl;
-    	cout<<"-------------------------"<<endl;
-    	cout<<"version:open version menu."<<endl;
-    	cout<<"help:open this menu."<<"\n";
-    	cout<<"cls:clean screen."<<endl;
+    		cout<<"helplist"<<endl;
+    		cout<<"-------------------------"<<endl;
+    		cout<<"version:open version menu."<<endl;
+    		cout<<"help:open this menu."<<"\n";
+    		cout<<"cls:clean screen."<<endl;
 		cout<<"reload:reload a windows command."<<endl; 
-    	cout<<">>>";
+		cout<<"exit:exit this program.";
+    		cout<<">>>";
 	}
 	else if(s=="version"){
-    	cout<<"Long Cyan Command Tools"<<endl;
-    	cout<<"Version 1.0.0"<<endl;
-    	cout<<"Test version 1.0.3.7"<<endl;
-    	cout<<"Beta version 1.0.2.3"<<endl;
-    	cout<<"Preview version 1.0.5.3"<<endl;
-    	cout<<">>>";
+    		cout<<"Long Cyan Command Tools"<<endl;
+    		cout<<"Version 1.0.0"<<endl;
+    		cout<<"Test version 1.0.3.7"<<endl;
+    		cout<<"Beta version 1.0.2.3"<<endl;
+    		cout<<"Preview version 1.0.5.3"<<endl;
+    		cout<<">>>";
 	}
 	else if(s==" "){
 		cout<<"Please don't insert solo space!"<<endl;
@@ -41,8 +42,11 @@ int take(string s){
 		cout<<">>>";
 
 	}
+	else if(s=="exit"){
+		return 0;
+	}
 	else{
 		cout<<"Please insert a command!"<<endl;
-    	cout<<">>>";
+    		cout<<">>>";
 	}
 }
