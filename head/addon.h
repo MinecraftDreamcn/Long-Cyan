679d1972480a7626018b32e270e88e340fedb142
MinecraftDreamCN <158718037+MinecraftDreamcn@users.noreply.github.com>
2025-04-27 12:53:25 +0800

    Add files via upload

---
+++ head/addon.h
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+#include <conio.h>
+using namespace std;
+bool i[5000000];
+bool switcher(string fn,int n);
+void use(string n){
+	if(n=="?")
+	cout<<"Import helplist\n-------------------------\nmath: download the math tools.\nccas: download the command can add space.\nmgr:launch addon manager.\n>>>";
+	else if(n=="mgr"){
+    	i[0]=1;
+        string t;
+        cout << "What Add-on do you want manage?" << endl;
+        if(i[1]!=0){
+        	cout<<"1:math\n"; 
+		}
+		else{
+			cout<<"1:math(off)\n";
+		}
+		if(i[2]!=0){
+        	cout<<"2:ccas\n"; 
+		}
+		else{
+			cout<<"2:ccas(off)\n";
+		}
+        cout << "-->";
+        getline(cin, t);
+        if(t=="1"){
+        	switcher("math",1); 
+		}
+		else if(t=="2"){
+			switcher("ccas",2);
+		}
+	}
+	else{
+		if(n=="math"&&i[1]!=0){
+		system("start addon\\math.exe");
+		cout<<">>>";
+		}
+		else if(n=="ccas"&&i[2]!=0){
+		system("start addon\\ccas.exe");
+		cout<<">>>";
+		}
+	}
+}
+bool switcher(string fn,int n) {
+    char input;
+    while(1){
+    	system("cls");
+    	cout<<"Switch the fuction:"<<fn;
+    cout << "[";
+    if (i[n]==1) {
+        cout << "ON";
+    }
+	else {
+        cout << "OFF";
+    }
+    cout << "] ";
+    cout<<"\n";
+    cout<<"Space:Switch switch           Q:Exit";
+    input=_getch();
+    if(input==32){
+    	i[n]=!i[n];
+    	cout<<"\a";
+	}
+	else if(input==81||input==113){
+		break;
+	}
+}
+	cout<<">>>";
+}
+
+int import(string n){
+	/*
+		0:mgr
+		1:mathtools
+		2:commandcanaddspace
+	*/ 
+	 if(n=="math"){
+		string url = "https://github.com/MinecraftDreamcn/LCY-Add_on/math.exe";
+    	string output_file = "addon\\math.exe";
+    	string command = "curl -s -o " + output_file + " " + url;
+    	int result = system(command.c_str());
+    	if (result != 0) {
+      		cerr << "Failed to download the Add-on." << endl;
+     		return 1;
+    	}
+    	i[1]=1;
+    	cout << "Import the fuction:Math!"<< endl;
+	}
+	else if(n=="ccas"){
+		string url = "https://github.com/MinecraftDreamcn/LCY-Add_on/ccas.exe";
+    	string output_file = "addon\\ccas.exe";
+    	string command = "curl -s -o " + output_file + " " + url;
+    	int result = system(command.c_str());
+    	if (result != 0) {
+      		cerr << "Failed to download the Add-on." << endl;
+     		return 1;
+    	}
+    	i[2]=1;
+    	cout << "Import the fuction:Command can add space!"<< endl;
+	}
+	return 0;
+}
--- head/lcystd.h
+++ head/lcystd.h
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
+#include"addon.h"
 using namespace std;
-
 int take(string s) {
+	string fn;
     if (s.empty()) {
         cout << ">>>";
         return 0;
@@ -13,15 +14,16 @@ int take(string s) {
         cout << "cls: clean screen." << endl;
         cout << "cmd: open cmd." << endl;
         cout << "reload: reload a windows command." << endl;
+        cout << "use:use a Add-on.\nimport: import a add-on ." << endl;
         cout << "exit: exit this program." << endl;
         cout << ">>>";
         return 0;
     } else if (s == "version") {
         cout << "Long Cyan Command Tools" << endl;
-        cout << "Version-1.3.0" << endl;
-        cout << "Test version-1.04.5.6" << endl;
-        cout << "Beta version-1.1.8.9" << endl;
-        cout << "Preview version-1.2.17.1" << endl;
+        cout << "Version-1.4.0" << endl;
+        cout << "Test version-1.07.5.6" << endl;
+        cout << "Beta version-1.4.8.9" << endl;
+        cout << "Preview version-1.5.17.1" << endl;
         cout << ">>>";
         return 0;
     } else if (s == "cls") {
@@ -47,7 +49,24 @@ int take(string s) {
         }
         cout << ">>>";
         return 0;
-    } else if (s == "exit") {
+    }
+    else if(s=="import"){
+		string t;
+		cout << "Insert ? to show helplist." << endl;
+        cout << "What Add-on do you want to use?" << endl;
+        cout << "-->";
+        getline(cin, t);
+        import(t);
+	}
+	else if(s=="use"){
+		string t;
+		cout << "Insert ? to show helplist." << endl;
+        cout << "What Add-on do you want to use?" << endl;
+        cout << "-->";
+        getline(cin, t);
+        use(t);
+	}
+	else if (s == "exit") {
         return 5;
     } else if (s == " ") {
         cout << "Please don't insert solo space!" << endl;
