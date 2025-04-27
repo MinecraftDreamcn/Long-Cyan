803375d5622e99824327754271686ccc2242a541
MinecraftDreamCN <158718037+MinecraftDreamcn@users.noreply.github.com>
2025-04-27 13:00:30 +0800

    Update lcy.cpp

--- lcy.cpp
+++ lcy.cpp
@@ -7,7 +7,7 @@ int main(){
   string s;
   cout<<"LCY Command-tool"<<endl;
   cout<<"Please update at https://github.com/MinecraftDreamcn/Long-Cyan"<<endl;
-  cout<<"Version 1.3.0 Release"<<endl;
+  cout<<"Version 1.4.0 Release"<<endl;
   cout<<"Insert 'help' to open help menu.Insert 'exit' to exit program."<<endl;
   cout<<">>>";
   while(getline(cin,s)){
@@ -19,7 +19,7 @@ int main(){
   Sleep(3000);
   cout<<"PROGRAM EXIT AFTER 15000MS,PLEASE DON'T WORRY!"<<endl;
   Sleep(3000);
-  cout<<"PROGRAM BY CONST TEAM(POWERED BY DWS),C++,ÕÔè÷³õ MAKE THIS.THIS IS A EGGSTER!"<<endl;
+  cout<<"PROGRAM BY CONST TEAM(POWERED BY DWS),C++,Ã•Ã”Ã¨Ã·Â³Ãµ MAKE THIS.THIS IS A EGGSTER!"<<endl;
   Sleep(3000);
   cout<<"Sleep(3000);"<<endl;
   Sleep(3000);
